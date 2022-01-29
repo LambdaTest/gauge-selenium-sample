5392806ff3f31d83d8ed3fc654be583b1b1b43d9
4msha <firdausia1345@gmail.com>
2022-01-29 14:40:40 +0530

    :added geolocation

--- src/test/java/driver/driver/DriverFactory.java
+++ src/test/java/driver/driver/DriverFactory.java
@@ -13,36 +13,38 @@ import java.net.URL;
 public class DriverFactory {
 
     // Get a new WebDriver Instance.
-    // There are various implementations for this depending on browser/OS. The required browser/OS can be set as an environment variable.
-    // Refer http://getgauge.io/documentation/user/current/managing_environments/README.html
+    // There are various implementations for this depending on browser/OS. The
+    // required browser/OS can be set as an environment variable.
+    // Refer
+    // http://getgauge.io/documentation/user/current/managing_environments/README.html
 
     private static final String username = System.getenv("LT_USERNAME");
     private static final String accesskey = System.getenv("LT_ACCESS_KEY");
     public static final String gridURL = "@hub.lambdatest.com/wd/hub";
     public String status = "passed";
 
-    private static WebDriver driver= null;
+    private static WebDriver driver = null;
 
     public static WebDriver getDriver() {
         return driver;
     }
 
-
     @BeforeSpec
     public void setUp() {
 
-    try{
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("browserName", System.getenv("BROWSER"));
-        capabilities.setCapability("version", System.getenv("BROWSER_VERSION"));
-        capabilities.setCapability("platform", System.getenv("PLATFORM"));
-        capabilities.setCapability("build", "Java Gauge Framework");
-        capabilities.setCapability("name", "Sample Gauge Test");
-        capabilities.setCapability("network", false); // To enable network logs
-        capabilities.setCapability("visual", false); // To enable step by step screenshot
-        capabilities.setCapability("console", false); // To capture console logs
+        try {
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setCapability("browserName", System.getenv("BROWSER"));
+            capabilities.setCapability("version", System.getenv("BROWSER_VERSION"));
+            capabilities.setCapability("platform", System.getenv("PLATFORM"));
+            capabilities.setCapability("build", "Java Gauge Framework");
+            capabilities.setCapability("name", "Sample Gauge Test");
+            capabilities.setCapability("geoLocation", "US");
+            capabilities.setCapability("network", false); // To enable network logs
+            capabilities.setCapability("visual", false); // To enable step by step screenshot
+            capabilities.setCapability("console", false); // To capture console logs
 
-        driver = new RemoteWebDriver(new URL("https://" + username + ":" + accesskey + gridURL), capabilities);
+            driver = new RemoteWebDriver(new URL("https://" + username + ":" + accesskey + gridURL), capabilities);
         } catch (MalformedURLException e) {
             System.out.println("Invalid grid URL");
         } catch (Exception e) {
@@ -50,7 +52,6 @@ public class DriverFactory {
         }
 
     }
-
 
     @AfterSpec
     public void tearDown() {
